127c40bce843b35151fc4b7e75d1757382603d4b
Jack McBride <mcbride.jack1@gmail.com>
2021-12-21 10:59:50 +0000

    ouchieiei

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.use(cors());
 let count;
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
 });
 
 app.get("/count", (req, res) => {
